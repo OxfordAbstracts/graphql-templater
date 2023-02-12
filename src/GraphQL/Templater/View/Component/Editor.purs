df7258130d83be2fb44ba84bbf558b77df629ba5
Rory Campbell <roryc89@googlemail.com>
2023-02-12 08:55:50 +0700

    moves to components

---
+++ src/GraphQL/Templater/View/Component/Editor.purs
@@ -0,0 +1,297 @@
+module GraphQL.Templater.View.Component.Editor
+  ( Completion(..)
+  , CompletionContext(..)
+  , CompletionResult
+  , CompletionSource
+  , Diagnostic
+  , EditorView(..)
+  , Input
+  , Match
+  , Output(..)
+  , Query(..)
+  , Severity(..)
+  , ViewUpdate(..)
+  , component
+  , explicit
+  , getViewContent
+  , getViewUpdateContent
+  , getViewUpdateSelectionRanges
+  , matchBefore
+  , setContent
+  )
+  where
+
+import Prelude
+
+import Control.Promise (Promise, fromAff)
+import Data.Array.NonEmpty (NonEmptyArray)
+import Data.Foldable (for_)
+import Data.Function.Uncurried (Fn4, mkFn4)
+import Data.Maybe (Maybe(..))
+import Data.Nullable (Nullable, notNull, null, toMaybe, toNullable)
+import Data.String.Regex (Regex)
+import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Aff.Class (class MonadAff)
+import Effect.Class (class MonadEffect)
+import Effect.Unsafe (unsafePerformEffect)
+import Halogen (RefLabel(..), liftEffect)
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+import Halogen.Subscription as HS
+import Web.DOM (Element)
+
+type Input =
+  { doc :: String
+  , lint :: Array Diagnostic
+  , autocompletion :: Maybe CompletionSource
+  }
+
+data Query a
+  = GetContent (Maybe String -> a)
+  | Relint (Array Diagnostic) a
+
+type State =
+  { input :: Input
+  , view :: Maybe EditorView
+  }
+
+data Action
+  = Init
+  | Receive Input
+  | HandleChange ViewUpdate
+  | HandleSelectionSet ViewUpdate
+
+data Output
+  = DocChanged ViewUpdate
+  | SelectionChanged ViewUpdate
+
+component :: forall m. MonadAff m => H.Component Query Input Output m
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval $ H.defaultEval
+        { handleAction = handleAction
+        , handleQuery = handleQuery
+        , initialize = Just Init
+        , receive = Just <<< Receive
+        }
+    }
+  where
+  initialState :: Input -> State
+  initialState =
+    { input: _
+    , view: Nothing
+    }
+
+  handleAction :: Action -> H.HalogenM _ _ _ _ m Unit
+  handleAction = case _ of
+    Init -> do
+      { input: input@{ doc, lint } } <- H.get
+      elMb <- H.getRef label
+      for_ elMb \parent -> do
+        handleChange <- H.liftEffect HS.create
+        onSelectionSet <- H.liftEffect HS.create
+        void $ H.subscribe handleChange.emitter
+        void $ H.subscribe onSelectionSet.emitter
+        view <- liftEffect $ makeView
+          { parent
+          , doc
+          , onChange: HS.notify handleChange.listener <<< HandleChange
+          , onSelectionSet: HS.notify onSelectionSet.listener <<< HandleSelectionSet
+          , lint: toForeignDiagnostic <$> lint
+          , autocomplete: toNullable $ completionSourceToForeign <$> input.autocompletion
+          }
+        H.modify_ _
+          { view = Just view
+          }
+
+    Receive input -> do
+      viewMb <- H.gets _.view
+      case viewMb of
+        Nothing -> pure unit
+        Just view ->
+          H.liftEffect $ reloadAutocompleteImpl
+            { view
+            , autocomplete: toNullable $ completionSourceToForeign <$> input.autocompletion
+            }
+
+    HandleChange viewUpdate -> H.raise $ DocChanged viewUpdate
+    HandleSelectionSet viewUpdate -> H.raise $ SelectionChanged viewUpdate
+
+  handleQuery
+    :: forall a
+     . Query a
+    -> H.HalogenM _ _ _ _ m (Maybe a)
+  handleQuery = case _ of
+    GetContent reply -> do
+      { view: viewMb } <- H.get
+      case viewMb of
+        Nothing -> pure $ Just $ reply Nothing
+        Just view -> do
+          content <- liftEffect $ getViewContent view
+          pure $ Just $ reply $ Just content
+
+    Relint lint reply -> do
+      { view } <- H.get
+      for_ view (relint lint)
+      pure $ Just reply
+
+  render :: State -> _
+  render _st = HH.div [ HP.ref label ] []
+
+  label = RefLabel "editor-parent"
+
+foreign import makeView
+  :: { parent :: Element
+     , doc :: String
+     , onChange :: ViewUpdate -> Effect Unit
+     , onSelectionSet :: ViewUpdate -> Effect Unit
+     , lint :: Array DiagnosticForeign
+     , autocomplete :: Nullable CompletionSourceForeign
+     }
+  -> Effect EditorView
+
+foreign import getViewContent :: EditorView -> Effect String
+
+foreign import setContent :: String -> EditorView -> Effect Unit
+
+foreign import getViewUpdateContent :: ViewUpdate -> Effect String
+
+foreign import getViewUpdateSelectionRanges :: ViewUpdate -> Effect (NonEmptyArray { from :: Int, to :: Int })
+
+relint :: forall m. MonadEffect m => Array Diagnostic -> EditorView -> m Unit
+relint lint view = liftEffect $ relintImpl { view, lint: toForeignDiagnostic <$> lint }
+
+foreign import relintImpl
+  :: { view :: EditorView
+     , lint :: Array DiagnosticForeign
+     }
+  -> Effect Unit
+
+foreign import reloadAutocompleteImpl
+  :: { view :: EditorView
+     , autocomplete :: Nullable CompletionSourceForeign
+     }
+  -> Effect Unit
+
+data EditorView
+
+-- | https://codemirror.net/docs/ref/#view.ViewUpdate
+data ViewUpdate
+
+type Diagnostic =
+  { from :: Int
+  , to :: Int
+  , severity :: Severity
+  , message :: String
+  }
+
+data Severity = Error | Warning | Info
+
+type DiagnosticForeign =
+  { from :: Int
+  , to :: Int
+  , severity :: String
+  , message :: String
+
+  }
+
+toForeignDiagnostic :: Diagnostic -> DiagnosticForeign
+toForeignDiagnostic d = d
+  { severity = case d.severity of
+      Error -> "error"
+      Warning -> "warning"
+      Info -> "info"
+  }
+
+-- Autocompletion
+
+type CompletionSource = CompletionContext -> Aff (Maybe CompletionResult)
+
+completionSourceToForeign :: CompletionSource -> CompletionSourceForeign
+completionSourceToForeign = map (map (map completionResultToForeign >>> toNullable) >>> fromAff)
+  where
+  completionResultToForeign :: CompletionResult -> CompletionResultForeign
+  completionResultToForeign r = r
+    { options = completionToForeign <$> r.options
+    }
+
+  completionToForeign :: Completion -> CompletionForeign
+  completionToForeign c = CompletionForeign
+    c
+      { detail = toNullable c.detail
+      , info = toNullable c.info
+      , type = toNullable c.type
+      , apply = case c.apply of
+          Nothing -> null
+          Just fn -> notNull $ mkFn4 \view _completion from to ->
+            unsafePerformEffect $ fn { view, from, to }
+      }
+
+type CompletionSourceForeign = CompletionContext -> Effect (Promise (Nullable CompletionResultForeign))
+
+data CompletionContext
+
+-- | https://codemirror.net/docs/ref/#autocomplete.CompletionContext.matchBefore
+matchBefore
+  :: forall m
+   . MonadEffect m
+  => Regex
+  -> CompletionContext
+  -> m
+       ( Maybe
+           { from :: Int
+           , text :: String
+           , to :: Int
+           }
+       )
+matchBefore rgx ctx = liftEffect $ toMaybe <$> matchBeforeImpl rgx ctx
+
+foreign import matchBeforeImpl :: Regex -> CompletionContext -> Effect (Nullable Match)
+foreign import explicit :: CompletionContext -> Boolean
+
+type Match =
+  { text :: String
+  , from :: Int
+  , to :: Int
+  }
+
+-- | https://codemirror.net/docs/ref/#autocomplete.CompletionResult
+type CompletionResult =
+  { from :: Int
+  , filter :: Boolean
+  , options :: Array Completion
+  }
+
+type CompletionResultForeign =
+  { from :: Int
+  , filter :: Boolean
+  , options :: Array CompletionForeign
+  }
+
+-- | https://codemirror.net/docs/ref/#autocomplete.Completion
+type Completion =
+  { label :: String
+  , detail :: Maybe String
+  , info :: Maybe String
+  , type :: Maybe String
+  , apply ::
+      Maybe
+        ( { view :: EditorView
+          , from :: Int
+          , to :: Int
+          }
+          -> Effect Unit
+        )
+  }
+
+newtype CompletionForeign = CompletionForeign
+  { label :: String
+  , detail :: Nullable String
+  , info :: Nullable String
+  , type :: Nullable String
+  , apply :: Nullable (Fn4 EditorView CompletionForeign Int Int Unit)
+  }
---
+++ src/GraphQL/Templater/View/Component/NestedDropdown.purs
@@ -0,0 +1,201 @@
+module GraphQL.Templater.View.Component.NestedDropdown
+  ( DropdownItem(..)
+  , Input
+  , nestedDropdown
+  ) where
+
+import Prelude
+
+import Data.Array (find, findMap, head, uncons)
+import Data.Lazy (Lazy, force)
+import Data.Maybe (Maybe(..))
+import Data.Monoid (guard)
+import Debug (traceM)
+import Effect.Class (class MonadEffect, liftEffect)
+import GraphQL.Templater.View.Html.Icons (chevronDown, chevronRight, chevronUp)
+import GraphQL.Templater.View.Html.Utils (css, whenElem)
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Web.Event.Event (stopPropagation)
+import Web.UIEvent.MouseEvent (MouseEvent)
+import Web.UIEvent.MouseEvent as MouseEvent
+
+data Action id
+  = Init
+  | Receive (Input id)
+  | ToggleOpen
+  | SetPath (Array id) MouseEvent
+  | Select (Array id)
+
+type Input id =
+  { label :: String
+  , items :: Lazy (Array (DropdownItem id))
+  }
+
+data DropdownItem id
+  = Node
+      { label :: String
+      , id :: id
+      }
+  | Parent
+      { label :: String
+      , children :: Lazy (Array (DropdownItem id))
+      , id :: id
+      , selectable :: Boolean
+      }
+
+type State id =
+  { input :: Input id
+  , open :: Boolean
+  , path :: Array id
+  }
+
+nestedDropdown
+  :: forall id m q
+   . MonadEffect m
+  => Eq id
+  => H.Component q (Input id) (Array id) m
+nestedDropdown =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval H.defaultEval
+        { handleAction = handleAction
+        , initialize = Just Init
+        }
+    }
+  where
+  initialState :: Input id -> State id
+  initialState =
+    { input: _
+    , open: false
+    , path: []
+    }
+
+  render :: State id -> HH.HTML _ (Action id)
+  render state@{ input: { items } } =
+    HH.div [ css "relative mt-1" ]
+      [ HH.button
+          [ css "relative w-full cursor-pointer rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm"
+          , HE.onClick \_ -> ToggleOpen
+          ]
+          [ HH.text state.input.label
+          , HH.span
+              [ css "pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2" ]
+              [ if state.open then chevronUp [] else chevronDown []
+              ]
+          ]
+      , whenElem state.open \_ ->
+          HH.div [ css "absolute flex z-10 mt-1 max-h-[32rem] w-auto " ] $
+            renderItems state [] state.path (force items)
+      ]
+
+  renderItems :: State id -> Array id -> Array id -> Array (DropdownItem id) -> Array (HH.HTML _ (Action id))
+  renderItems state currentPath pathToFollow items =
+    [ HH.ul
+        [ css "overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm" ]
+        $ items <#> \item ->
+            let
+              itemCss selectable selected =
+                "text-gray-900 relative flex w-full justify-between select-none py-2 pl-3 pr-3 "
+                  <> guard selectable " cursor-pointer"
+                  <> (if selected then " bg-blue-200" else " hover:bg-blue-100")
+
+            in
+              case item of
+                Node { label, id } ->
+                  HH.li
+                    [ css $ itemCss true (isSelected id)
+                    , HE.onClick \_ -> Select $ currentPath <> [ id ]
+                    , HE.onMouseEnter (SetPath currentPath)
+                    ]
+                    [ HH.text label
+                    ]
+
+                Parent { label, selectable, id } ->
+                  let
+                    newPath = currentPath <> [ id ]
+                  in
+                    HH.li
+                      ( [ css $ itemCss true (isSelected id)
+                        , HE.onMouseEnter (SetPath newPath)
+                        ]
+                          <> guard selectable
+                            [ HE.onClick \_ -> Select newPath
+                            ]
+                      )
+                      [ HH.div [] [ HH.text label ]
+                      , chevronRight []
+                      ]
+    ]
+      <> case childrenMay of
+        Just { children, id, tail } ->
+          renderItems state (currentPath <> [ id ]) tail (force children)
+        _ -> []
+
+    where
+    isSelected :: id -> Boolean
+    isSelected id = Just id == head pathToFollow
+
+    childrenMay = case uncons pathToFollow of
+      Just { head, tail } ->
+        items # findMap case _ of
+          Parent { id, children } | head == id -> Just { children, tail, id }
+          _ -> Nothing
+      _ -> Nothing
+
+  -- <div class="relative mt-1">
+  --   <button type="button" class="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm" aria-haspopup="listbox" aria-expanded="true" aria-labelledby="listbox-label">
+  --     <span class="block truncate">Tom Cook</span>
+  --     <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+  --       <!-- Heroicon name: mini/chevron-up-down -->
+  --       <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+  --         <path fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clip-rule="evenodd" />
+  --       </svg>
+  --     </span>
+  --   </button>
+  -- <ul class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm" tabindex="-1" role="listbox" aria-labelledby="listbox-label" aria-activedescendant="listbox-option-3">
+  --   <!--
+  --     Select option, manage highlight styles based on mouseenter/mouseleave and keyboard navigation.
+
+  --     Highlighted: "text-white bg-blue-600", Not Highlighted: "text-gray-900"
+  --   -->
+  --   <li class="text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9" id="listbox-option-0" role="option">
+  --     <!-- Selected: "font-semibold", Not Selected: "font-normal" -->
+  --     <span class="font-normal block truncate">Wade Cooper</span>
+
+  --     <!--
+  --       Checkmark, only display for selected option.
+
+  --       Highlighted: "text-white", Not Highlighted: "text-blue-600"
+  --     -->
+  --     <span class="text-blue-600 absolute inset-y-0 right-0 flex items-center pr-4">
+  --       <!-- Heroicon name: mini/check -->
+  --       <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+  --         <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+  --       </svg>
+  --     </span>
+  --   </li>
+
+  --   <!-- More items... -->
+  -- </ul>
+
+  handleAction :: (Action id) -> H.HalogenM (State id) (Action id) _ _ m Unit
+  handleAction = case _ of
+    Init -> pure unit
+
+    Receive input ->
+      pure unit
+
+    ToggleOpen -> H.modify_ \st -> st { open = not st.open }
+
+    SetPath path ev -> do
+      traceM "SetPath"
+      traceM { path }
+      liftEffect $ stopPropagation $ MouseEvent.toEvent ev
+      H.modify_ _ { path = path }
+
+    Select path -> H.raise path
+-- H.modify_ \_state -> input
+-- nestedDropdown :: Array ()
